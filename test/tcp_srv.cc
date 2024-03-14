95af38629122ce6526439301a3753e1f28e7f784
yjl <1264163816@qq.com>
2024-03-14 23:44:19 +0800

    EventLoop与TimerWheel联合调试成功

--- test/tcp_srv.cc
+++ test/tcp_srv.cc
@@ -1,20 +1,20 @@
 #include "../source/server.hpp"
 void HandleClose(Channel *channel){
-     std::cout<<"close: "<<channel->Fd()<<std::endl;
+     DBG_LOG("close fd :%d",channel->Fd());
      channel->Remove();
      delete channel;
 }
 void HandleRead(Channel *channel){
     int fd=channel->Fd();
     char buf[1024]={0};
+    printf("sockfd:%d\n",fd);
     int ret =recv(fd,buf,1023,0);
     if(ret <= 0){
         // 如果 recv 返回 0，表示对端关闭了连接
         // 如果返回值小于 0，则表示发生了错误
-        std::cerr << "recv failed with error: " << strerror(errno) << std::endl;
         return HandleClose(channel);
     }
-    std::cout<<buf<<std::endl;
+    DBG_LOG("%s",buf);
     channel->EnableWrite();//启动可写事件
 }
 void HandleWrite(Channel *channel){
@@ -24,9 +24,6 @@ void HandleWrite(Channel *channel){
     if(ret < 0){
         std::cerr << "send failed with error: " << strerror(errno) << std::endl;
         return HandleClose(channel); // 关闭释放
-    } else {
-        std::cout << "发送成功" << std::endl;
-        std::cout.flush(); // 确保立即输出
     }
     channel->DisableWrite(); // 关闭写监控
 }
@@ -34,37 +31,38 @@ void HandleWrite(Channel *channel){
 void HandleError(Channel *channel){
     return HandleClose(channel);//关闭释放
 }
-void HandleEvent(Channel *channel){
-    std::cout<<"有一个事件到来"<<std::endl;
+void HandleEvent(EventLoop *loop,Channel *channel,uint64_t timerid){
+     loop->TimerRefresh(timerid);
 }
-void Accepter(Poller *poller,Channel *lst_channel){
+void Accepter(EventLoop *loop,Channel *lst_channel){
     int fd =lst_channel->Fd();
     int newfd=accept(fd,NULL,NULL);
-    if(newfd <0 )return ;
-    Channel *channel=new Channel(poller,newfd);
+    if(newfd <0 ) {return ;}
+    uint64_t timerid =rand()%10000;
+    Channel *channel=new Channel(loop,newfd);
     channel->SetReadCallback(std::bind(HandleRead,channel));//为通信套接字设置可读事件的回调函数
     channel->SetWriteCallback(std::bind(HandleWrite,channel));//可写事件的回调函数
     channel->SetCloseCallback(std::bind(HandleClose,channel));//关闭事件回调函数
     channel->SetErrorCallback(std::bind(HandleError,channel));
-    channel->SetEventCallback(std::bind(HandleEvent,channel));
+    channel->SetEventCallback(std::bind(HandleEvent,loop,channel,timerid));
+    //非活跃连接的超时释放操作,10s后关闭连接
+    //注意：定时销毁任务，必须在启动读事件之前，因为有可能启动事件监控后，立即就有事件，但是这时候还没有任务
+    loop->TimerAdd(timerid,10,std::bind(HandleClose,channel));
     channel->EnableRead();
 }
 int main()
 {   
-    Poller poller;
+    srand(time(NULL));
+    EventLoop loop;
     Socket lst_sock;
     lst_sock.CreateServer(8000);
     //为监控套接字，创建应该channel进行事件的管理，以及事件的处理
-    Channel channel(&poller,lst_sock.Fd());
+    Channel channel(&loop,lst_sock.Fd());
     //回调中，获取新连接，为新连接创建Channel并添加监控
-    channel.SetReadCallback(std::bind(Accepter,&poller,&channel));
-    channel.EnableRead();//启动可读事件监控
+    channel.SetReadCallback(std::bind(Accepter,&loop,&channel));
+    channel.EnableRead();//启   动可读事件监控
     while(1){
-      std::vector<Channel*> actives;
-      poller.Poll(&actives);
-      for(auto &a :actives){
-        a->HandleEvent();//开始监控
-      }
+     loop.Start();
     }
     lst_sock.Close();
     return 0;
