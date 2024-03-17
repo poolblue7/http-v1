def14ce546a94a45449f677b39f10d2cd3d0cc49
yjl <1264163816@qq.com>
2024-03-17 15:00:18 +0800

    Acceptor模块设计与实现

--- source/server.hpp
+++ source/server.hpp
@@ -1139,6 +1139,39 @@ private:
             _loop->RunInLoop(std::bind(&Connection::UpgradeInLoop, this, context, conn, msg, closed, event));
         }
 };
+class Acceptor
+{ 
+    private: 
+          Socket    _socket;
+          EventLoop *_loop;
+          Channel  _channel;
+
+          using AcceptCallback=std::function<void(int)>;
+          AcceptCallback _accept_callback;
+    private:
+    /*监听套接字的读事件回调处理函数---获取新连接，调用_accept_callback函数进行新连接处理*/
+    void HandleRead(){
+        int newfd=_socket.Accept();
+        if(newfd <0){
+            return;
+        }
+        if(_accept_callback) {_accept_callback(newfd);}  
+    }
+    int CreateServer(int port){
+       bool ret =_socket.CreateServer(port);
+       assert(ret==true);
+       return _socket.Fd();
+    }
+    public:
+    /*不能将启动读事件监控，放到构造函数中，必须在设置回调函数后，再去启动*/
+     /*否则有可能造成启动监控后，立即有事件，处理的时候，回调函数还没设置：新连接得不到处理，且资源泄漏*/
+    Acceptor(EventLoop *loop,int port):_socket(CreateServer(port)),_loop(loop),_channel(_loop,_socket.Fd()){
+         _channel.SetReadCallback(std::bind(&Acceptor::HandleRead,this));
+    }
+     void SetAcceptCallback(const AcceptCallback &cb){ _accept_callback=cb;}
+     void Listen() { _channel.EnableRead(); }
+};
+   
 void Channel::Remove() { return _loop->RemoveEvent(this); }
 void Channel::Update() { return _loop->UpdateEvent(this); }
 void TimerWheel::TimerAdd(uint64_t id, uint32_t delay, const TaskFunc &cb)
