d4d573e118670dfe1d8407cd475697f256c1d722
yjl <1264163816@qq.com>
2024-03-17 12:12:13 +0800

    Connection模块联调成功

--- test/tcp_srv.cc
+++ test/tcp_srv.cc
@@ -1,54 +1,35 @@
 #include "../source/server.hpp"
-void HandleClose(Channel *channel){
-     DBG_LOG("close fd :%d",channel->Fd());
-     channel->Remove();
-     delete channel;
+std::unordered_map<uint64_t,PtrConnection> _conns;
+uint64_t conn_id =0;
+void ConnectionDestory(const PtrConnection &conn){
+    _conns.erase(conn->Id());
 }
-void HandleRead(Channel *channel){
-    int fd=channel->Fd();
-    char buf[1024]={0};
-    printf("sockfd:%d\n",fd);
-    int ret =recv(fd,buf,1023,0);
-    if(ret <= 0){
-        // 如果 recv 返回 0，表示对端关闭了连接
-        // 如果返回值小于 0，则表示发生了错误
-        return HandleClose(channel);
-    }
-    DBG_LOG("%s",buf);
-    channel->EnableWrite();//启动可写事件
-}
-void HandleWrite(Channel *channel){
-    int fd = channel->Fd();
-    const char *data = "测试成功！";
-    int ret = send(fd, data, strlen(data), 0);
-    if(ret < 0){
-        std::cerr << "send failed with error: " << strerror(errno) << std::endl;
-        return HandleClose(channel); // 关闭释放
-    }
-    channel->DisableWrite(); // 关闭写监控
+void OnMessage(const PtrConnection &conn,Buffer *buf){
+   DBG_LOG("%s",buf->ReadPosition());
+   buf->MoveReadOffset(buf->ReadAbleSize());
+   std::string str="Hello World";
+   conn->Send(str.c_str(),str.size());
+   
 }
 
-void HandleError(Channel *channel){
-    return HandleClose(channel);//关闭释放
-}
-void HandleEvent(EventLoop *loop,Channel *channel,uint64_t timerid){
-     loop->TimerRefresh(timerid);
+
+
+void OnConnected(const PtrConnection &conn){
+    DBG_LOG("NEW CONNECTION:%p",conn.get());
 }
 void Accepter(EventLoop *loop,Channel *lst_channel){
     int fd =lst_channel->Fd();
     int newfd=accept(fd,NULL,NULL);
     if(newfd <0 ) {return ;}
-    uint64_t timerid =rand()%10000;
-    Channel *channel=new Channel(loop,newfd);
-    channel->SetReadCallback(std::bind(HandleRead,channel));//为通信套接字设置可读事件的回调函数
-    channel->SetWriteCallback(std::bind(HandleWrite,channel));//可写事件的回调函数
-    channel->SetCloseCallback(std::bind(HandleClose,channel));//关闭事件回调函数
-    channel->SetErrorCallback(std::bind(HandleError,channel));
-    channel->SetEventCallback(std::bind(HandleEvent,loop,channel,timerid));
-    //非活跃连接的超时释放操作,10s后关闭连接
-    //注意：定时销毁任务，必须在启动读事件之前，因为有可能启动事件监控后，立即就有事件，但是这时候还没有任务
-    loop->TimerAdd(timerid,10,std::bind(HandleClose,channel));
-    channel->EnableRead();
+    conn_id++;
+    PtrConnection conn(new Connection(loop,conn_id,newfd));
+    conn->SetMessageCallback(std::bind(OnMessage,std::placeholders::_1,std::placeholders::_2));
+    conn->SetConnectedCallback(std::bind(OnConnected,std::placeholders::_1));
+    conn->SetSrvClosedCallback(std::bind(ConnectionDestory,std::placeholders::_1));
+    conn->EnableInactiveRelease(10);//启动非活跃超时销毁
+    conn->Established();//就绪初始化
+    _conns.insert(std::make_pair(conn_id,conn));
+    
 }
 int main()
 {   
