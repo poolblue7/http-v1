a8e87e3240d840d4491a97dd77958ded43bc7925
yjl <1264163816@qq.com>
2024-03-17 22:53:09 +0800

    LoopThread模块和LoopThreadPool模块测试成功

--- test/tcp_srv.cc
+++ test/tcp_srv.cc
@@ -1,7 +1,8 @@
 #include "../source/server.hpp"
 std::unordered_map<uint64_t,PtrConnection> _conns;
+EventLoop base_loop;
 uint64_t conn_id =0;
-EventLoop loop;
+LoopThreadPool * loop_pool;
 void ConnectionDestory(const PtrConnection &conn){
     _conns.erase(conn->Id());
 }
@@ -20,28 +21,29 @@ void OnConnected(const PtrConnection &conn){
 }
 void NewConnection(int fd){
     conn_id++;
-    PtrConnection conn(new Connection(&loop,conn_id,fd));
+    PtrConnection conn(new Connection(loop_pool->NextLoop(),conn_id,fd));
     conn->SetMessageCallback(std::bind(OnMessage,std::placeholders::_1,std::placeholders::_2));
     conn->SetConnectedCallback(std::bind(OnConnected,std::placeholders::_1));
     conn->SetSrvClosedCallback(std::bind(ConnectionDestory,std::placeholders::_1));
     conn->EnableInactiveRelease(10);//启动非活跃超时销毁
     conn->Established();//就绪初始化
     _conns.insert(std::make_pair(conn_id,conn));
+    DBG_LOG("主线程--------------");
     
 }
 int main()
 {   
     srand(time(NULL));
-    
+    loop_pool=new LoopThreadPool(&base_loop);
+    loop_pool->SetThreadCount(2);
+    loop_pool->Create();
     Socket lst_sock;
-    Acceptor acceptor(&loop,8000);
-
+    Acceptor acceptor(&base_loop,8000);
+    
     //回调中，获取新连接，为新连接创建Channel并添加监控
     acceptor.SetAcceptCallback(std::bind(NewConnection,std::placeholders::_1));
     acceptor.Listen();
-    while(1){
-     loop.Start();
-    }
-
+     base_loop.Start();
+ 
     return 0;
 }
