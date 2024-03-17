3b59780ca40aaadaa1e807c616dfa2dd330eab00
yjl <1264163816@qq.com>
2024-03-17 14:59:19 +0800

    Acceptor模块联调成功

--- test/tcp_srv.cc
+++ test/tcp_srv.cc
@@ -1,6 +1,7 @@
 #include "../source/server.hpp"
 std::unordered_map<uint64_t,PtrConnection> _conns;
 uint64_t conn_id =0;
+EventLoop loop;
 void ConnectionDestory(const PtrConnection &conn){
     _conns.erase(conn->Id());
 }
@@ -17,12 +18,9 @@ void OnMessage(const PtrConnection &conn,Buffer *buf){
 void OnConnected(const PtrConnection &conn){
     DBG_LOG("NEW CONNECTION:%p",conn.get());
 }
-void Accepter(EventLoop *loop,Channel *lst_channel){
-    int fd =lst_channel->Fd();
-    int newfd=accept(fd,NULL,NULL);
-    if(newfd <0 ) {return ;}
+void NewConnection(int fd){
     conn_id++;
-    PtrConnection conn(new Connection(loop,conn_id,newfd));
+    PtrConnection conn(new Connection(&loop,conn_id,fd));
     conn->SetMessageCallback(std::bind(OnMessage,std::placeholders::_1,std::placeholders::_2));
     conn->SetConnectedCallback(std::bind(OnConnected,std::placeholders::_1));
     conn->SetSrvClosedCallback(std::bind(ConnectionDestory,std::placeholders::_1));
@@ -34,17 +32,16 @@ void Accepter(EventLoop *loop,Channel *lst_channel){
 int main()
 {   
     srand(time(NULL));
-    EventLoop loop;
+    
     Socket lst_sock;
-    lst_sock.CreateServer(8000);
-    //为监控套接字，创建应该channel进行事件的管理，以及事件的处理
-    Channel channel(&loop,lst_sock.Fd());
+    Acceptor acceptor(&loop,8000);
+
     //回调中，获取新连接，为新连接创建Channel并添加监控
-    channel.SetReadCallback(std::bind(Accepter,&loop,&channel));
-    channel.EnableRead();//启   动可读事件监控
+    acceptor.SetAcceptCallback(std::bind(NewConnection,std::placeholders::_1));
+    acceptor.Listen();
     while(1){
      loop.Start();
     }
-    lst_sock.Close();
+
     return 0;
 }
